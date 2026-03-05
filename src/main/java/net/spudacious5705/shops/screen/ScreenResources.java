f69cacbac6618acfbf424d5455c20c7eee910be0
Spudacious <spud.dev5705@gmail.com>
2026-03-05 14:57:11 +0000

    Added tooltip for empty payment/product slots

--- src/main/java/net/spudacious5705/shops/screen/ScreenResources.java
+++ src/main/java/net/spudacious5705/shops/screen/ScreenResources.java
@@ -43,8 +43,10 @@ public class ScreenResources {
     public static final MutableComponent NONE = Component.translatable("gui.spudaciousshops.text_none");
     public static final MutableComponent CREATIVE_TOGGLE_TOOLTIP = Component.translatable("gui.spudaciousshops.toggle_creative");
     public static final MutableComponent EFFECTS_TOGGLE_TOOLTIP = Component.translatable("gui.spudaciousshops.toggle_effects");
-    public static final MutableComponent SHOP_STYLE_TOGGLE_TOOLTIP = Component.translatable("gui.spudaciousshops.toggle_shop_style");//todo add translations
+    public static final MutableComponent SHOP_STYLE_TOGGLE_TOOLTIP = Component.translatable("gui.spudaciousshops.toggle_shop_style");
     public static final MutableComponent IGNORE_NBT_TOGGLE_TOOLTIP = Component.translatable("gui.spudaciousshops.toggle_ignore_nbt");
+    public static final MutableComponent PAYMENT_EMPTY_TOOLTIP = Component.translatable("gui.spudaciousshops.payment_empty_tooltip");
+    public static final MutableComponent PRODUCT_EMPTY_TOOLTIP = Component.translatable("gui.spudaciousshops.product_empty_tooltip");
 
     public static final MutableComponent STOCK = Component.translatable("gui.spudaciousshops.stock");
     public static final MutableComponent REGISTER = Component.translatable("gui.spudaciousshops.register");
